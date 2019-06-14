2383281b17b6ff2ade1ba298cf8a0cd6e253df64
Vasil Vasilev <vaskovasilev94@yahoo.com>
2019-06-14 10:40:00 +0300

    Searching for hotels.

--- iface/cli.py
+++ iface/cli.py
@@ -17,9 +17,16 @@ class Cli(ChatIface):
         pass
 
     def on_start(self, user_id):
-        dialog = self.get_dialog(user_id)
-        r = dialog.start()
-        logging.info('Text: %s', r)
+        d = self.get_dialog(user_id)
+        r = d.start()
+        d.on_searching(lambda details: self._callback_on_searching(user_id, details))
         return r
 
-
+    def _callback_on_searching(self, user_id, details):
+        logging.info("Starting search at %s", details)
+        place = details['location'].capitalize()
+        t = details['type']
+        if len(t) == 0:
+            logging.info("Great. Here are a few places for you to stay in {0}..".format(place))
+        else:
+            logging.info("Great. I'll start searching for {0} in {1}..".format('placest to stay', place))
